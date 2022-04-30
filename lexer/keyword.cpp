8c7338164c8d8e44af4ab635836a9c6cf0dfd146
slaakko <slaakko@gmail.com>
2022-04-30 18:33:03 +0300

    util

---
+++ lexer/keyword.cpp
@@ -0,0 +1,44 @@
+// =================================
+// Copyright (c) 2022 Seppo Laakko
+// Distributed under the MIT license
+// =================================
+
+module lexer.keyword;
+
+namespace lexer {
+
+inline const char32_t* StrEnd(const char32_t* s)
+{
+    while (*s)
+    {
+        ++s;
+    }
+    return s;
+}
+
+KeywordMap::KeywordMap(const Keyword* keywords_) : keywords(keywords_)
+{
+    const Keyword* kw = keywords;
+    while (kw->str)
+    {
+        Lexeme lexeme(kw->str, StrEnd(kw->str));
+        keywordMap[lexeme] = kw->tokenID;
+        ++kw;
+    }
+}
+
+int KeywordMap::GetKeywordToken(const Lexeme& lexeme) const
+{
+    std::map<Lexeme, int, LexemeComparer>::const_iterator it = keywordMap.find(lexeme);
+    if (it != keywordMap.cend())
+    {
+        return it->second;
+    }
+    else
+    {
+        return INVALID_TOKEN;
+    }
+}
+
+
+} // namespace lexer
