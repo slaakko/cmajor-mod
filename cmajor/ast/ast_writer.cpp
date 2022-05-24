77a2ff653f91a49302eda273dc5780c501b05872
Olli Myöhänen <olli.myohanen@welho.com>
2022-05-24 22:41:50 +0300

    ast reader and writer

---
+++ cmajor/ast/ast_writer.cpp
@@ -0,0 +1,52 @@
+// =================================
+// Copyright (c) 2022 Seppo Laakko
+// Distributed under the MIT license
+// =================================
+/*
+#include <sngcm/ast/AstWriter.hpp>
+#include <sngcm/ast/AstReader.hpp>
+#include <sngcm/ast/Node.hpp>
+#include <boost/uuid/nil_generator.hpp>
+*/
+
+module cmajor.ast.writer;
+
+import cmajor.ast.node;
+
+namespace cmajor::ast {
+
+//AstWriter::AstWriter(const std::string& fileName_) : binaryStreamWriter(fileName_), lexers(nullptr), spanConversionModuleId(boost::uuids::nil_uuid())
+    AstWriter::AstWriter(const std::string& fileName_) : fileStream(fileName_,util::OpenMode::binary| util::OpenMode::write), bufferedStream(fileStream), binaryStreamWriter(bufferedStream)
+{
+}
+
+void AstWriter::Write(Node* node)
+{
+    binaryStreamWriter.Write(static_cast<uint8_t>(node->GetNodeType()));
+    binaryStreamWriter.Write(node->ModuleId());
+    node->Write(*this);
+}
+
+void AstWriter::Write(Specifiers specifiers)
+{
+    binaryStreamWriter.Write(static_cast<uint32_t>(specifiers));
+}
+
+void AstWriter::Write(const soul::lexer::SourcePos& sourcePos)
+{
+    binaryStreamWriter.WriteULEB128UInt(static_cast<uint32_t>(sourcePos.file));
+    binaryStreamWriter.WriteULEB128UInt(static_cast<uint32_t>(sourcePos.line));
+    binaryStreamWriter.WriteULEB128UInt(static_cast<uint32_t>(sourcePos.col));
+}
+/*
+void AstWriter::SetLexers(std::vector<soulng::lexer::Lexer*>* lexers_)
+{
+    lexers = lexers_;
+}
+
+void AstWriter::SetSpanConversionModuleId(const boost::uuids::uuid& spanConversionModuleId_)
+{
+    spanConversionModuleId = spanConversionModuleId_;
+}
+*/
+} // namespace cmajor::ast
---
+++ cmajor/ast/ast_writer.ixx
@@ -0,0 +1,44 @@
+
+// =================================
+// Copyright (c) 2022 Seppo Laakko
+// Distributed under the MIT license
+// =================================
+export module cmajor.ast.writer;
+
+/*
+#include <sngcm/ast/AstApi.hpp>
+#include <sngcm/ast/Specifier.hpp>
+#include <soulng/util/BinaryWriter.hpp>
+#include <soulng/lexer/Span.hpp>
+#include <soulng/lexer/Lexer.hpp>
+*/
+import std.core;
+import util;
+import cmajor.ast.specifier;
+import soul.lexer.source.pos;
+
+export namespace cmajor::ast {
+
+class Node;
+
+class AstWriter
+{
+public:
+    AstWriter(const std::string& fileName_);
+    util::BinaryStreamWriter& GetBinaryStreamWriter() { return binaryStreamWriter; }
+    void Write(Node* node);
+    void Write(Specifiers specifiers);
+    void Write(const soul::lexer::SourcePos& sourcePos);
+    //void SetLexers(std::vector<soulng::lexer::Lexer*>* lexers_); TODO
+    //void SetSpanConversionModuleId(const boost::uuids::uuid& spanConversionModuleId_); TODO
+    //const boost::uuids::uuid& SpanConversionModuleId() const { return spanConversionModuleId; } TODO
+private:
+    util::FileStream fileStream;
+    util::BufferedStream bufferedStream;
+    util::BinaryStreamWriter binaryStreamWriter;
+    //std::vector<soulng::lexer::Lexer*>* lexers;
+    //boost::uuids::uuid spanConversionModuleId;
+};
+
+} // namespace cmajor::ast
+
