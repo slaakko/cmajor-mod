b6f8674a5d2e175529d05aec7200ca4f39ddcd78
Olli Myöhänen <olli.myohanen@xorrit.fi>
2023-05-09 22:18:47 +0300

    xml_attribute_processor.cpp, uuid_repository.cpp

---
+++ cmajor/binder/uuid_repository.cpp
@@ -0,0 +1,33 @@
+module cmajor.binder.uuid.repository;
+// =================================
+// Copyright (c) 2022 Seppo Laakko
+// Distributed under the MIT license
+// =================================
+
+//#include <cmajor/binder/UuidRepository.hpp>
+import util;
+
+namespace cmajor::binder {
+
+int UuidRepository::Install(const util::uuid& uuid)
+{
+    auto it = uuidMap.find(uuid);
+    if (it != uuidMap.cend())
+    {
+        return it->second;
+    }
+    else
+    {
+        int id = uuids.size();
+        uuidMap[uuid] = id;
+        uuids.push_back(uuid);
+        return id;
+    }
+}
+
+const util::uuid& UuidRepository::GetUuid(int id) const
+{
+    //Assert(id >= 0 && id < uuids.size(), "invalid uuid id");
+    return uuids[id];
+}
+} // namespace cmajor::binder
