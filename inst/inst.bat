749af86ceffcb667494e33c801e7dc422426e363
slaakko <slaakko@gmail.com>
2024-12-22 16:47:32 +0200

    cm/masm

--- inst/inst.bat
+++ inst/inst.bat
@@ -22,4 +22,4 @@ cminst -v ..\cmajor\projects\tools\ref2html\bin\llvm\release\ref2html.exe ..\cma
 cminst -v ..\cmajor\projects\tools\soulcm\cm2html\bin\llvm\release\cm2html.exe ..\cmajor\bin
 cminst -v ..\cmajor\projects\tools\soulcm\soul2html\bin\llvm\release\soul2html.exe ..\cmajor\bin
 cminst -v ..\cmajor\projects\tools\txt2html\bin\llvm\release\txt2html.exe ..\cmajor\bin
-cminst -v D:\src\cmajor-mod-5.0-scm\cmajor\projects\tools\compiler\cmcmc\lib\cm\release\0\cmcmc.exe ..\cmajor\bin
+cminst -v D:\src\cmajor-mod-5.0-scm\cmajor\projects\tools\compiler\cmcmc\lib\cm\release\1\cmcmc.exe ..\cmajor\bin
